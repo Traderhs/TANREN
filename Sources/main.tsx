d85b9a7b7e346c3435eb6aa18d7f8133ee97e228
Haseung Ryu <haseung.ryu.ai@gmail.com>
2026-09-01 22:23:01 +0900

    chore(ui): add book and typography dependencies

--- Sources/main.tsx
+++ Sources/main.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "pretendard/dist/web/variable/pretendardvariable-dynamic-subset.css";
+import "@fontsource-variable/geist/wght.css";
+import "@fontsource/zen-kaku-gothic-new/400.css";
+import "@fontsource/zen-kaku-gothic-new/500.css";
+import "@fontsource/zen-kaku-gothic-new/700.css";
 import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
